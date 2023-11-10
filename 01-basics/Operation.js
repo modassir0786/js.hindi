f73f0f85d3547605a1fa712ad5f5d0d9f6aeca9f
modassir0786 <148438660+modassir0786@users.noreply.github.com>
2023-11-10 10:49:32 +0530

    Add files via upload

---
+++ 01-basics/Operation.js
@@ -0,0 +1,32 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//console.log(3+3);
+//console.log(3-3);
+let str1 = "hello"
+let str2 = "modassir"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log( 3+ 4 * 5 % 3);
+
+
+
+console.log(+true);
+console.log(+"");
+
+
+
+
+let gameCounter = 100
+gameCounter++; 
+console.log(gameCounter);
+
+
+
+
