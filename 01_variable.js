da8bd0ae6b317d6616fabfa2b39a2f31a9b93f12
modassir0786 <148438660+modassir0786@users.noreply.github.com>
2023-11-10 10:44:35 +0530

    Add files via upload

---
+++ 01_variable.js
@@ -0,0 +1,14 @@
+console.log("modassir")
+const accountId = 12363
+let accountEmail="mdmodassir3436@gmail.com"
+var  accoutPassword ="0786"
+accountCity = "patna "
+accountState;
+
+// accountId = 2 not allowed
+accountEmail="modassir@gmail.com"
+accoutPassword ="1234556"
+accountCity = "darbhanga"
+
+console.log(accountId);
+console.table([accountId, accountEmail,accoutPassword,accountCity,accountState])
